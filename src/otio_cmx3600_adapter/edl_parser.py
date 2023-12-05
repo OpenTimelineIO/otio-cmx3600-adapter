4fd04c347b8866a8ad9f1ec944b5d2e496df46f7
Eric Reinecke <ereinecke@netflix.com>
2023-12-05 14:26:06 -0800

    Fixed parsing for some edge case standard form lines
    Signed-off-by: Eric Reinecke <ereinecke@netflix.com>

--- src/otio_cmx3600_adapter/edl_parser.py
+++ src/otio_cmx3600_adapter/edl_parser.py
@@ -4,9 +4,11 @@ from typing import Iterable, Iterator
 
 from .edl_statement import (
     EDLStatement,
+    EffectType,
     NoteFormStatement,
     StandardFormStatement,
 )
+from .exceptions import EDLParseError
 
 
 # This set of regexes tries to follow the robustness principle - especially
@@ -21,6 +23,9 @@ EDIT_NUMBER_RE = re.compile(
 COMMENT_RE = re.compile(
     r"^\*\s*(?P<data>.*)"
 )
+
+VALID_EFFECT_TYPES = {et.value for et in EffectType}
+
 
 # These known identifier types are parsed into note form statements, all others
 # are parsed into UnhandledStatements - they'll be treated like comments
@@ -103,24 +108,72 @@ def statements_from_lines(edl_lines: Iterable[str]) -> Iterator[EDLStatement]:
         fields = list(
             field for field in SEPARATOR_RE.split(consuming_line) if field
         )
-        if not did_have_edit_number or len(fields) < 7 or len(fields) > 8:
+        if not did_have_edit_number:
             yield _note_form_statement_from_line(line, **element_context)
             continue
 
+        if len(fields) < 6 or len(fields) > 8:
+            raise EDLParseError(
+                f"incorrect number of fields [{len(fields)}] in line number:"
+                f" {line_number} statement: {line}"
+            )
+
+        # consume the fields from the head of the line
+        consuming_fields = fields[:]
+        source_identification = consuming_fields[0]
+        channels = consuming_fields[1]
+        edit_type = consuming_fields[2]
+        for i in range(3):
+            if i == 0:
+                source_identification = consuming_fields.pop(0)
+            elif i == 1:
+                channels_candidate = consuming_fields[0]
+                if channels_candidate in VALID_EFFECT_TYPES:
+                    # This indicates the channel assignment has no whitespace
+                    # delimiter between the reel name and it (this can happen in
+                    # cases with maxing out fixed column width reel names
+                    # Here we'll try to infer reel width based on fixed-width
+                    # reel names.
+                    if len(source_identification) > 32:
+                        # File 32 EDL
+                        reel_width = 32
+                    elif len(source_identification) > 16:
+                        # File 16 EDL
+                        reel_width = 16
+                    else:
+                        # EDL Classic (TM)
+                        reel_width = 8
+                    # If the channel doesn't look valid, see if it might have
+                    # mushed into the end of the reel
+                    channels = source_identification[reel_width:]
+                    source_identification = source_identification[:reel_width]
+                else:
+                    channels = channels_candidate
+                    consuming_fields.pop(0)
+            elif i == 2:
+                edit_type = consuming_fields.pop(0)
+
+        # Consume the record and source fields from the end of the line back
+        rec_out = consuming_fields.pop()
+        rec_in = consuming_fields.pop()
+        src_out = consuming_fields.pop()
+        src_in = consuming_fields.pop()
+
         # Parse as a standard form element
         standard_statement_fields = dict(
-            source_identification=fields[0],
-            channels=fields[1],
-            edit_type=fields[2],
-            source_entry=fields[-4],
-            source_exit=fields[-3],
-            sync_entry=fields[-2],
-            sync_exit=fields[-1],
+            source_identification=source_identification,
+            channels=channels,
+            edit_type=edit_type,
+            source_entry=src_in,
+            source_exit=src_out,
+            sync_entry=rec_in,
+            sync_exit=rec_out,
             is_supported=True,
         )
 
-        if len(fields) == 8:
-            standard_statement_fields["edit_parameter"] = fields[3]
+        # If the edit parameter was present, add it
+        if consuming_fields:
+            standard_statement_fields["edit_parameter"] = consuming_fields.pop()
 
         standard_statement_fields.update(element_context)
         yield StandardFormStatement(**standard_statement_fields)
